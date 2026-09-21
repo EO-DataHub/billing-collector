ed0718567b084d50467e5eb74dd2490295deb793
Dave Dowding <ddowding@sparkgeo.com>
2026-09-21 14:23:05 +0100

    Revert "chore: log Prometheus queries through logging instead of print"
    This reverts commit c7302470b01c6c5e2b571d3ab1f2d68ac1dabae8.

--- billing_collector/billing_messager.py
+++ billing_collector/billing_messager.py
@@ -60,7 +60,7 @@ class ResourceUsageMessager(PulsarJSONMessager[BillingEvent, BillingEvent]):
         """
         Query Prometheus for a range of data including historical.
         """
-        logging.debug("Querying Prometheus: from %s to %s with step %s", start, end, step)
+        print(f"Querying Prometheus: from {start} to {end} with step {step}")
         resp = requests.get(
             f"{self.prometheus_url}/api/v1/query_range",
             params={
